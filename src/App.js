fe985d1f5cf06f97a9ef8052faba0be4fcac5837
Ahmed Yassen <ahmedyassen46@outlook.com>
2023-10-28 16:52:32 +0200

    analytics

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import "./App.css";
 import TodoWrapper from "./components/TodoWrapper";
 import Todo from "./components/Todo";
@@ -5,6 +6,7 @@ import Todo from "./components/Todo";
 function App() {
   return <div className="App">
     <TodoWrapper></TodoWrapper>
+    <Analytics/>
   </div>;
 }
 
