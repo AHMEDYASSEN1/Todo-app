88f9dbac89f006d2f1621ddc4e5cd22567135be3
Ahmed Yassen <ahmedyassen46@outlook.com>
2023-10-29 16:00:19 +0200

    icon-commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Analytics } from '@vercel/analytics/react';
 import "./App.css";
 import TodoWrapper from "./components/TodoWrapper";
 import Todo from "./components/Todo";
 
 function App() {
-  return <div className="App">
-    <TodoWrapper></TodoWrapper>
-    <Analytics/>
-  </div>;
+  return (
+    <div className="App">
+      <TodoWrapper></TodoWrapper>
+    </div>
+  );
 }
 
 export default App;
